55d807f4ab049450c5593659637b1148d635392e
MeliLiz <92534481+MeliLiz@users.noreply.github.com>
2024-03-18 18:37:05 -0600

    Update Vertice.cs

--- IAAspiradora/Assets/Scripts/Graphs/Vertice.cs
+++ IAAspiradora/Assets/Scripts/Graphs/Vertice.cs
@@ -2,50 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Vertice
-{
+public class Vertice{
 
     public List<Vertice> vecinos = new List<Vertice>();
     public int id;
+
+    public float f, g, h;
     public bool visitado;
     public Vertice padre;
     public Vertice camino;
     public Vector3 posicion;
 
-    public float f, g, h;
-
-    public Vertice(int newId, Vector3 newPos)
-    {
+    public Vertice(int newId, Vector3 newPos) {
         id = newId;
         posicion = newPos;
         visitado = false;
+        f = g = h = float.MaxValue;
     }
 
-    public void setPadre(Vertice padre)
-    {
+    public void setPadre(Vertice padre) {
         this.padre = padre;
     }
 
-    public void setVisitado(bool visitado)
-    {
+    public void setVisitado(bool visitado) {
         this.visitado = visitado;
     }
 
-    public void AgregarVecino(Vertice newVertice)
-    {
+    public void AgregarVecino(Vertice newVertice) {
         vecinos.Add(newVertice);
     }
 
     //M�todo que convierte a cadena el v�rtice, contiene su ID, ID de vecinos y padre.
-    public string toString()
-    {
-        string aux = id.ToString() + ":";
-        foreach (Vertice v in vecinos)
-        {
+    public string toString() {
+        string aux = id.ToString()+":";
+        foreach(Vertice v in vecinos) {
             aux += v.id + ",";
         }
-        if (padre != null)
-        {
+        if (padre != null) {
             aux += "P:" + padre.id;
         }
         return aux;
